62960041ab3f1a6a3dc704561cd6bc6fcbb66928
kingsleyadam <kingsleyadam@msn.com>
2016-02-19 12:14:59 +0100

    Clone and change default page back to index

--- Kingsley.io/App_Start/RouteConfig.cs
+++ Kingsley.io/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Kingsley.io
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Splash", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
